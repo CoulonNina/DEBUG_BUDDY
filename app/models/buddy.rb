478619213d79c5e684d5d4b7dd3326b568218d97
Nina Coulon <ninacoulon@ymail.com>
2022-05-24 12:14:20 +0200

    buddy model validates

--- app/models/buddy.rb
+++ app/models/buddy.rb
@@ -1,4 +1,7 @@
 class Buddy < ApplicationRecord
+  SKILLS = ["Ruby", "Javascript", "HTML/CSS", "Python", "PHP"]
+  validates :description, :skills,  presence: :true
+  validates :skills, inclusion: { in: SKILLS }
   belongs_to :user
   has_many :bookings
 end

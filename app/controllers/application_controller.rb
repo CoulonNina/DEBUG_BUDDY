fd75e408e76c99157cc41897255c7f00b5d36e83
Alice Palussiere <journalice@gmail.com>
2022-05-27 15:22:28 +0200

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
 
   after_action :verify_authorized, except: :index,  unless: :skip_pundit?
   after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
+  add_flash_types :success
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb

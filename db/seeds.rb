aa47fc8867696f0c3b85611ec5b4a80fd0a37573
ophelia ondella <ophelia.ondella@gmail.com>
2022-05-27 12:44:58 +0200

    Ajout Buddy Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -81,6 +81,20 @@ user10 = User.create!(
     password: "azerty"
   )
 
+  user11 = User.create!(
+    email: "Marjoline@gmail.com",
+    first_name: "Marjoline",
+    last_name: "Bernard",
+    password: "azerty"
+  )
+
+  user12 = User.create!(
+    email: "Mila@testmail.com",
+    first_name: "Mila",
+    last_name: "Kadjy",
+    password: "azerty"
+  )
+
 puts 'Finished!'
 
 puts 'Creating buddies...'
@@ -106,7 +120,7 @@ buddy3 = Buddy.new(
     description: "Au cours de ces 5 dernières années en tant que chargée de communication avec des entreprises B2B et B2C, j'ai développé et mis en place des stratégies de communication digitales qui ont permis d'obtenir de meilleurs taux d'engagement et de conversion. Au sein de l’équipe Marketing chez Sharing, j'ai augmenté le taux d'engagement du compte Instagram de 23 %. Je souhaite mettre mes compétences en stratégie de la communication et des réseaux sociaux en tant que Social Media Manager chez ConnectPhase afin de booster votre taux de conversion et créer un contenu de qualité à travers la mise en place d'une stratégie d'A/B testing performante.",
     user: user3
   )
-buddy3.photo.attach(io: URI.open("https://res.cloudinary.com/dgw2zajw1/image/upload/v1653044199/cld-sample.jpg"), filename: "image")
+buddy3.photo.attach(io: URI.open("https://res.cloudinary.com/dvtuelr2w/image/upload/v1653645518/ez45jwj5q1cjuktoskuc.jpg"), filename: "image")
 buddy3.save
 
 buddy4 = Buddy.new(
@@ -122,7 +136,7 @@ buddy5 = Buddy.new(
   description: "Hotel receptionist with a Bachelor’s degree from Vatel Hotel Management School, and one year of experience split in two hotels (luxurious and family hotels). Fluent in English, French and German. Great presentation, adaptation and courtesy helped hotels reach their highest satisfaction rate and largest number of five-star reviews during my two work placements. Eager to bring my organisational skills to Aloft London Excel.",
   user: user5
 )
-buddy5.photo.attach(io: URI.open("https://res.cloudinary.com/dgw2zajw1/image/upload/v1653044199/cld-sample.jpg"), filename: "image")
+buddy5.photo.attach(io: URI.open("https://res.cloudinary.com/dvtuelr2w/image/upload/v1653645147/images_o7nfcc.jpg"), filename: "image")
 buddy5.save
 
 buddy6 = Buddy.new(
@@ -130,11 +144,11 @@ buddy6 = Buddy.new(
   description: "3 ans chez Rakuten, en tant que chef de projet marketing e-commerce et assistante marketing (secteur mode et beauté). J’y ai augmenté le nombre de visiteurs de 30% et passé le taux de conversion de 1,8% à 4,2%. Maîtrisant les supports du marketing digital, réseaux sociaux et contenu (CMS et Google Analytics), je peux permettre à votre boutique de mode en ligne d’exploser en nombre de visiteurs et d’acheteurs.",
   user: user6
 )
-buddy6.photo.attach(io: URI.open("https://res.cloudinary.com/dgw2zajw1/image/upload/v1653044199/cld-sample.jpg"), filename: "image")
+buddy6.photo.attach(io: URI.open("https://res.cloudinary.com/dvtuelr2w/image/upload/v1653645051/studio-portrait-of-attractive-20-year-old-bearded-man-picture-id1296696829_skavuh.jpg"), filename: "image")
 buddy6.save
 
 buddy7 = Buddy.new(
-  skills: "Bootstrap",
+  skills: "Ruby",
   description: "Suite à une reconversion professionnelle, j'ai souhaité me diriger vers le domaine qui me passionne depuis longtemps: le développement web. J'ai donc suivi une formation débouchant sur le titre RNCP de niveau 3 de concepteur/développeur web.
 
   J'ai aujourd'hui plus de 4 ans d'expérience dans le développement web.
@@ -142,11 +156,11 @@ buddy7 = Buddy.new(
   N'hésitez pas à me contacter.",
   user: user7
 )
-buddy7.photo.attach(io: URI.open("https://res.cloudinary.com/dgw2zajw1/image/upload/v1653044199/cld-sample.jpg"), filename: "image")
+buddy7.photo.attach(io: URI.open("https://res.cloudinary.com/dvtuelr2w/image/upload/v1653647124/qz460jf1rerduxedsgvw.jpg"), filename: "image")
 buddy7.save
 
 buddy8 = Buddy.new(
-  skills: "UX/UI",
+  skills: "Javascript",
   description: "Cela fait maintenant 4 ans que je me suis spécialisé dans la création de site internet afin de pouvoir développer au mieux vos projets !
 
   Autodidacte et très investi, j’ai le plaisir de vous proposer mes services en tant que web développeur et web master(Création de site internet, design, référencement, rédaction web, Community Manager…).
@@ -155,25 +169,42 @@ buddy8 = Buddy.new(
   Alors n’hésitez plus, contactez-moi dès maintenant afin de faire décoller votre projet.",
   user: user8
 )
-buddy8.photo.attach(io: URI.open("https://res.cloudinary.com/dgw2zajw1/image/upload/v1653044199/cld-sample.jpg"), filename: "image")
+buddy8.photo.attach(io: URI.open("https://res.cloudinary.com/dvtuelr2w/image/upload/v1653645107/ijdo2q49xulnudjmmlyv.jpg"), filename: "image")
 buddy8.save
 
 buddy9 = Buddy.new(
-  skills: "Javascript",
+  skills: "HTML/CSS",
   description: "Spécialisé dans le développement front end, je suis en charge de l’interface utilisateur d’un site web ou d’une application. Mon métier nécessite des compétences techniques pointues, doublées d’un véritable sens créatif. N'hésitez pas à me consulter pour toute demande de projet, je mettrais mes qualités sur le fond comme sur la forme à votre service.
   Je sais aussi gérer la gestion de projet car je suis quelqu'un d'organisé.",
   user: user9
 )
-buddy9.photo.attach(io: URI.open("https://res.cloudinary.com/dgw2zajw1/image/upload/v1653044199/cld-sample.jpg"), filename: "image")
+buddy9.photo.attach(io: URI.open("https://res.cloudinary.com/dvtuelr2w/image/upload/v1653644740/wvcvxyvzrzno9b5wynex.jpg"), filename: "image")
 buddy9.save
 
 buddy10 = Buddy.new(
-  skills: "jQuery",
+  skills: "Ruby",
   description: "Développeur full stack certifié Scrum, avec plus de 2 ans d’expérience. Mordu d’informatique, j’ai appris à coder dès mon plus jeune âge dans divers langages informatiques (Javascript, PHP, AngularJS…) et ai créé des projets personnels, dont un site référençant les vidéos les plus vues sur la plateforme Twitch (+ 5000 visites/mois). Polyvalent, je maîtrise les différentes étapes techniques de la création d’un site ou d’une application web ; de la compréhension des besoins utilisateurs, au développement frontend et backend en passant par la maintenance.",
   user: user10
 )
-buddy10.photo.attach(io: URI.open("https://res.cloudinary.com/dgw2zajw1/image/upload/v1653044199/cld-sample.jpg"), filename: "image")
-buddy10.sav4
+buddy10.photo.attach(io: URI.open("https://res.cloudinary.com/dvtuelr2w/image/upload/v1653644845/AI_Face_w8twm5.jpg"), filename: "image")
+buddy10.save
+
+buddy11 = Buddy.new(
+  skills: "HTML/CSS",
+  description: "Specialized in front end development, I am in charge of the user interface of a website or an application. My job requires high technical skills, coupled with a real creative sense. Do not hesitate to contact me for any project request, I will put my qualities on the content as well as on the form at your service.
+  I also know how to manage projects because I am an organized person.",
+  user: user11
+)
+buddy11.photo.attach(io: URI.open("https://res.cloudinary.com/dvtuelr2w/image/upload/v1653647930/depositphotos_22256751-stock-photo-curly-hair-woman_jfd8pu.webp"), filename: "image")
+buddy11.save
+
+buddy12 = Buddy.new(
+  skills: "Ruby",
+  description: "Scrum certified full stack developer, with more than 2 years of experience. I learned to code from a young age in various computer languages (Javascript, PHP, AngularJS...) and created personal projects, including a site referencing the most viewed videos on the Twitch platform (+ 5000 visits/month). Versatile, I master the different technical steps of the creation of a website or a web application; from the understanding of the users' needs, to the frontend and backend development and the maintenance.",
+  user: user12
+)
+buddy12.photo.attach(io: URI.open("https://res.cloudinary.com/dvtuelr2w/image/upload/v1653648126/portrait-femme-affaires-etonnee-tenant-ordinateur-portable-regardant-camera-visage-etonne-utilisant-ordinateur-pour-etudier-travaillant-ligne-choquee-par-application-tourne-studio-isole-fond-rose_416530-21840_rpgxwd.jpg"), filename: "image")
+buddy12.save
 puts 'Finished!'
 
 puts 'Creating bookinks...'
@@ -190,7 +221,7 @@ Booking.create!(
     start_date: Date.yesterday,
     end_date: Date.today,
   )
-  
+
 Booking.create!(
     user_id: user3.id,
     buddy_id: buddy1.id,
